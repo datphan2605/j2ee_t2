6c2c5d5ca9c50c4e14d76122031789a2d5dae71c
datphan2605 <phandat26052k4@gmail.com>
2026-01-30 22:52:54 +0700

    up

---
+++ src/main/java/com/example/BaiTuan2/Controllers/test.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.BaiTuan2.Controllers;
+
+/**
+ *
+ * @author son03
+ */
+public class test {
+
+}
